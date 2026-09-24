3e85303702c66d10daf2819482739f6389a0ca01
minamiya-saki <mioyukinko@gmail.com>
2026-09-24 22:03:02 +0900

    9/24進捗

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
-// $(function () {
-//     $(".hamburger").on("click", function (){
-//         $(".header__nav--sp").fadeToggle();
-//         $(".hamburger").toggleClass("open");
-//     });
-// });
-
-
 $(function () {
     $(".hamburger").on("click", function (){
         $(".header__nav--sp").toggleClass("open");
         $(".hamburger").toggleClass("open");
+        $(".hamburger__overlay").toggleClass("open");
     });
 });
