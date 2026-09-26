e23520f84f9fd343858bc2e4a50364c6b59bc6c2
minamiya-saki <mioyukinko@gmail.com>
2026-09-26 23:23:54 +0900

    9/26__進捗

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ハンバーガーメニュー
 $(function () {
     $(".hamburger").on("click", function (){
         $(".header__nav--sp").toggleClass("open");
@@ -5,3 +6,24 @@ $(function () {
         $(".hamburger__overlay").toggleClass("open");
     });
 });
+
+// トップページ＿＿ボイス
+new Splide(".splide", {
+    autoplay: true,
+    type: "loop",
+    pauseOnHover: false, 
+    rewind: true,
+    interval: 8000,
+    speed: 800,
+    padding: "20%",
+    gap: 100,
+    fixedWidth: "600px",
+    focus: "center",
+    breakpoints: {
+        768: {
+            padding: "18.5%",
+            gap: 20,
+            fixedWidth: "300px",
+        },
+    },
+}).mount();
